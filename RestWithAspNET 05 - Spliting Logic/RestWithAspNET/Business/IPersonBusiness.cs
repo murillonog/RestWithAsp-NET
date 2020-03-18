e49041db2927fa741a9e9531f67d7d5d7914d583
Murillo Nogueira Home <murillo.nogueira01@gmail.com>
2020-03-18 18:39:23 -0300

    Implementing Spliting Logic

---
+++ RestWithAspNET 05 - Spliting Logic/RestWithAspNET/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithAspNET.Model;
+using System.Collections.Generic;
+
+namespace RestWithAspNET.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
